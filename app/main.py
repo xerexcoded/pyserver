fc769cdcda73926b2975a2f6f21aa9cf9cdc3ba2
xerexcoded <arnavbanerjee61@gmail.com>
2024-06-27 01:06:33 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -8,9 +8,9 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True) # reuse_port=True is needed for multiple clients
+    # server_socket.accept() # wait for client
+    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n") # send data to client, by accepting the connection and sending data
 
 if __name__ == "__main__":
     main()
